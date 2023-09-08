a0f9f4ea9c2a400e9cded80399b6327d02dbd1a9
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-09-08 17:17:07 -0500

    Se instalan las dependencias de desarrollo para trabajar con CSS, las dependencias son Tailwindcss, Postcss, Postcss-cli y autoprefixer

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,13 +1,11 @@
 import express from 'express';
+import { formularioLogin, formularioRegistro } from '../controllers/usuarioController.js';
 
 const router = express.Router();
 
 //Routing res.render sirve para imprimir una vista con un template engine
-router.get('/login', (req, res) => {
-    res.render('auth/login', {
-        autenticado: false,
-    });
-});
+router.get('/login', formularioLogin);
+router.get('/registro', formularioRegistro);
 
 
 export default router;
