29f481f7b9f5434e8e7c3ce48527db5cab29a166
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-09-08 23:21:22 -0500

    Se crean registros en la base de datos de usuarios registrados en la plataforma a través del formulario diseñado en la vista de registro

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -18,17 +18,47 @@ const registrar = async (req, res) => {
     // Validaciones
     await check('nombre').notEmpty().withMessage('El nombre es obligatorio').run(req)
     await check('email').isEmail().withMessage('El correo es invalido').run(req)
-    await check('password').isLength({ min: 6 }).withMessage('El password es incorrecto, debe contener más de 6 carácteres').run(req)
-    await check('repetir_password').equals('password').withMessage('La contraseña no es la misma').run(req)
+    await check('password').isLength({ min: 6 }).withMessage('El password es incorrecto, debe contener más de 6 caracteres').run(req);
+    await check('repetir_password').equals(req.body.password).withMessage('La contraseña no es la misma').run(req);
+
     let resultado = validationResult(req)
 
-    // Verificar que el resultado este vacío.
 
-    res.json(resultado.array());
+    // Verificar que el resultado este vacío.
+    if(!resultado.isEmpty()){
+        // Hay errores
+        return res.render('auth/registro', {
+            pagina: 'Crear Cuenta',
+            errores: resultado.array(),
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
+        });
+    }
+
+    // Extraer los datos
+    const { nombre, email, password} = req.body
+
+    //Verificar que el usuario no este duplicado
+    const existeUsuario = await Usuario.findOne( { where : { email: email } } )
+    if(existeUsuario){
+            return res.render('auth/registro', {
+            pagina: 'Crear Cuenta',
+            errores: [{msg: 'El usuario ya esta Registrado'}],
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
+        });
+    }
+
+    console.log(existeUsuario)
+
+    return;
 
     const usuario = await Usuario.create(req.body)
 
-    res.json(usuario)
 }
 
 const formularioOlvidePasword = (req, res) => {
