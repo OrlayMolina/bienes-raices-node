3271cb9541fc912f4257bca10a47cb1361ec591a
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-09-09 12:56:35 -0500

    Se configuran las variables de entorno para el envio de email

--- helpers/emails.js
+++ helpers/emails.js
@@ -1,4 +1,15 @@
+import nodemailer from 'nodemailer'
+
 const emaillRegistro = asyn(datos) => {
+
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
+        }
+      });
 
 }
 
