ff10b0374788b72bbcbfe66c5481259e4c01cace
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-09-10 23:54:27 -0500

    Se agrega un mapa al proyecto

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,1 +1,13 @@
-alert('Soy el mapa')
+(function() {
+
+    const lat = 4.566654;
+    const lng = -75.6477259;
+    const mapa = L.map('mapa').setView([lat, lng ], 16);
+    
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+
+})()
