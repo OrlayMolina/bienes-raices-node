64492d775eb1f15e992b079b343bb4d3907e3192
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-09-11 10:36:59 -0500

    Se agrega el leaflet de openAstreetMap para mostrar el mapa en donde el usuario indicará donde queda la propiedad que a va vender

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,13 +1,41 @@
 (function() {
 
-    const lat = 4.566654;
-    const lng = -75.6477259;
+    const lat = 4.5366299918557615;
+    const lng = -75.67180996559179;
     const mapa = L.map('mapa').setView([lat, lng ], 16);
+    let marker;
+
+    //Utilizar Provider y Geocoder  
+    const geocodeService  = L.esri.Geocoding.geocodeService();
     
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
 
+    //El pin  L contiene toda la insformación
+    marker = new L.marker([lat, lng], {
+        draggable: true, // para poder mover el pin
+        autoPan: true //Cuando mueva el pin se vuelva a centrar el mapa
+    })
+    .addTo(mapa)
+
+    //Detectar el movimiento del pin 
+    marker.on('moveend', e => {
+        marker = e.target
+
+        const posicion  = marker.getLatLng();
+
+        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng))
+
+        //Obtener la información de las calles al soltar el leaflet
+        geocodeService.reverse().latlng(posicion, 13).run(function(error, resultado) {
+
+            marker.bindPopup(resultado.address.LongLabel)
+
+            //Llenar los campos, como no siempre existe, le colocamos un string vacio
+            codument.querySelector('.calle').textContent = resultado.address.Address ?? '';
+        })
+    })
 
 })()
