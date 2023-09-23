84c86068d3f9913edf652fc07b2b4d97d3e31240
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-09-22 21:55:34 -0500

    Se la redireccion al dar clic en el logo

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,6 +1,7 @@
 import { unlink } from 'node:fs/promises'
 import { validationResult } from 'express-validator'
 import { Precio, Categoria, Propiedad } from '../models/index.js'
+import { esVendedor } from '../helpers/index.js'
 
 const admin = async (req, res) => {
 
@@ -328,6 +329,8 @@ const mostrarPropiedad = async (req, res) => {
 
     const { id } = req.params;
 
+    console.log(req.usuario)
+
     //Comprobar que la propiedad exista
     const propiedad = await Propiedad.findByPk(id, {
         include: [
@@ -343,7 +346,9 @@ const mostrarPropiedad = async (req, res) => {
     res.render('propiedades/mostrar', {
         propiedad,
         pagina: propiedad.titulo,
-        csrfToken: req.csrfToken() 
+        csrfToken: req.csrfToken(),
+        usuario: req.usuario,
+        esVendedor: esVendedor(req.usuario?.id, propiedad.usuarioId) 
     })
 }
 
